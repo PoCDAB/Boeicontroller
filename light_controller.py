0d4426d07d9caf88f5872fb78575074a76727e49
Wiebesu <caglar_G@live.nl>
2020-12-02 14:48:47 +0100

    Added better read + regex

--- light_controller.py
+++ light_controller.py
@@ -2,6 +2,10 @@ import RPi.GPIO as GPIOimport
 import spidev
 import time
 import os
+import ast 
+import re
+import mmap
+
 from time import sleep
 
 gpio.setwarnings(False)
@@ -22,22 +26,31 @@ def read_channel(channel):
     return data
 
 # arbitrary light level check for the LDR sensor
-def check_light_level():
-    return True if read_channel(0) > 940 else False
+def check_light_level(defaults=940):
+    return True if read_channel(0) > defaults else False
 
+# function to find the specific line of code in a transmission from the receiver
+def instructons_ops():
+    pattern = re.compile(b"\\[\\d\\d\\d,\\d\\d?\\d?,\\d\\d?\\d?\\]") 
+    w
+    with open('receiver_transmission.txt', 'rb', 0) as file,\
+    mmap.mmap(file.fileno(), 0, access=mmap.ACCESS_READ) as s:
+        if re.search(pattern, s):
+            data = s[:9]
+            return ast.literal_eval(data.decode('utf-8')) # list format: [light level, on-time, off-time]
+
+# function to prevent the receiver_transmission.txt to go out of control in size
 def file_ops():
-    with open('receiver_transmission.txt', 'r') as f:
-        contents = f.read()
-        instruct
-        pass #TODO put the specific data here which should be read and passed to the variables
-        
-#format lightlevel eg 940, on delay 4, sleep delay,
+    if os.path.isfile('receiver_transmission.txt'):
+        if os.stat('receiver_transmission.txt') > 1024:
+            os.remove('receiver_transmission.txt')
 
 
-while True:
-    if check_light_level():
-        gpio.output(, gpio.HIGH)
-        sleep()
-    gpio.Output(7, gpio.LOW)
-    sleep(30)
+def main(defaults=25):
+    while True:
+        if check_light_level():
+            gpio.output(7, gpio.HIGH)
+            sleep(defaults - 20])
+        gpio.Output(7, gpio.LOW)
+        sleep(defaults)
 
